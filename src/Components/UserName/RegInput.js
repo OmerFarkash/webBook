cbf193f3c6d6a145b3d7556522697dc432f7debd
Omer Farkash <146092653+OmerFarkash@users.noreply.github.com>
2024-02-07 15:51:10 +0200

    functionality + desin for gerestaration page

---
+++ src/Components/UserName/RegInput.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import './regInput.css';
+
+const RegInput = (props) => {
+  const [focused, setFocused] = useState(false);
+  const { label, errorMessage, onChange, id, ...inputProps } = props;  
+  
+  const handlefocus = (e) => {
+    setFocused(true);
+  };
+
+  return (
+    <div class="input-group">
+      <label>{label}</label>
+        <input
+          {...inputProps}
+          onChange={onChange} 
+          onBlur={handlefocus} 
+          onFocus={() =>
+            inputProps.name === "passwordConfirmation" && setFocused(true)
+          }
+          focused={focused.toString()}/>
+        <span>{errorMessage}</span>
+    </div>
+  );
+};
+
+export default RegInput;
--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,7 +1,83 @@
+import { useState } from "react";
 import "./register.css";
 import { Link } from "react-router-dom";
+import RegInput from "../../Components/UserName/RegInput.js";
 
 const Register = () => {
+  const [values, setValues] = useState({
+    name: "",
+    username: "",
+    email: "",
+    password: "",
+    passwordConfirmation: "",
+  });
+  
+  console.log("re-rendered");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+  }
+
+  const inputs = [
+    {
+      id: 1,
+      name: "Full name",
+      type: "text",
+      placeholder: "Full Name",
+      errorMessage: "Full name must be 3-16 characters long, it contains only English letters.",
+      label: "Full Name",
+      pattern: "^[a-zA-Z]{3,16}$",
+      required: true,
+    },
+    {
+      id: 2,
+      name: "User name",
+      type: "text",
+      placeholder: "User Name",
+      errorMessage: "User name must be 3-16 characters long, it contains only English letters and numbers.",
+      label: "User Name",
+      pattern: "^[a-zA-Z0-9]{3,16}$",
+      required: true,
+    },
+    {
+      id: 3,
+      name: "email",
+      type: "email",
+      placeholder: "Email@email.com",
+      errorMessage: "Enter a valid email address",
+      label: "Email",
+      required: true,
+    },
+    {
+      id: 4,
+      name: "password",
+      type: "password",
+      placeholder: "Password",
+      errorMessage: "password must be between 8-16 characters long, contain at least one number, " +
+       "one uppercase and one lowercase letter and one special character.",
+      label: "Password",
+      pattern: "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,16})",
+      required: true,
+    },
+    {
+      id: 5,
+      name: "passwordConfirmation",
+      type: "password",
+      placeholder: "Password Confirmation",
+      errorMessage: "Passwords do not match.",
+      label: "Password Confirmation",
+      pattern: values.password,
+      required: true,
+    },
+  ]
+
+  const onChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+  console.log(values);
+
   return (
     <div className="register">
       <div className="card">
@@ -14,25 +90,10 @@ const Register = () => {
         </div>
         <div className="left">
           <h1>Register</h1>
-          <form>
-            <div class="input-group">
-              <input
-                type="text"
-                aria-label="First name"
-                class="form-control"
-                placeholder="First Name"
-              />
-              <input
-                type="text"
-                aria-label="Last name"
-                class="form-control"
-                placeholder="Last Name"
-              />
-            </div>
-            <input type="text" placeholder="Username" />
-            <input type="email" placeholder="Email@email.com" />
-            <input type="password" placeholder="Password" />
-            <input type="password" placeholder="Password Confirmation" />
+          <form onSubmit={handleSubmit}>
+            {inputs.map((input) => (
+            <RegInput key={input.id} {...input} value={values[input.name]} onChange={onChange} /> 
+            ))}
             <button id="registerBtn">Register</button>
           </form>
         </div>
