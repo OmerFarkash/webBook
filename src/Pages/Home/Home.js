f4ecd4ecc8fb02d981dd1b4cce3af9faabbe049b
Omer Farkash <146092653+OmerFarkash@users.noreply.github.com>
2024-02-08 19:50:28 +0200

    use can see the user after login on home page

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,10 +2,13 @@ import "./home.css";
 import LeftBar from "../../Components/LeftBar/LeftBar.js";
 import NavBar from "../../Components/NavBar/NavBar.js";
 import Feed from "../../Components/Feed/Feed.js";
-
+import UserContext from '../../UserContext.js';
+import { useContext } from "react";
 
 const Home = () => {
-
+  const { user } = useContext(UserContext);
+  // user has the user object - apply what you need from it
+  console.log(user);
   return (
     <div className="Home">
       <NavBar />
