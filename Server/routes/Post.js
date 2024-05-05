c7734c86144b7f23fd9a2ed40ccb0a8610106b90
Omer <omerfarkash@gmail.com>
2024-05-05 18:44:19 +0300

    getUserPosts - return posts for  profile page
    getFeed - return the feed of any user

--- Server/routes/Post.js
+++ Server/routes/Post.js
@@ -4,8 +4,8 @@ const tokenController = require('../controllers/Token');
 const express = require('express');
 var router = express.Router();
 
-// Route to get a specific post by name
-router.get('/', tokenController.verifyToken, postController.getPost);
+// Route to Feed
+router.get('/', tokenController.verifyToken, postController.getFeed);
 
 // Route to like / remove like a post - works
 router.post('/:id/likes', tokenController.verifyToken, postController.likePost);
